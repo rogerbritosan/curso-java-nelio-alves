98335088af22cd16e4e1949e62922c45f87b70ef
Roger <rogerbritosan@gmail.com>
2023-01-26 16:07:19 -0300

    adicionando comentários

--- CursoJavaNelioAlves/src/estrutura/sequencial/EntradaDados1_1.java
+++ CursoJavaNelioAlves/src/estrutura/sequencial/EntradaDados1_1.java
@@ -15,6 +15,14 @@ public class EntradaDados1_1 {
 		int x;
 		double y;
 		
+		// digitar tudo na mesma linha com espaço
+		// ou apertar enter a cada entrada de dados
+		// exemplo:
+		// Roger 39 10
+		// Roger <enter>
+		// 39 <enter>
+		// 10 <enter>
+		
 		texto = sc.next();
 		x = sc.nextInt();
 		y = sc.nextDouble(); // não esquecer de digitar o float no formato conforme o idioma do computador, nesse caso ex: 5,4 -> imprimirá 5.4
