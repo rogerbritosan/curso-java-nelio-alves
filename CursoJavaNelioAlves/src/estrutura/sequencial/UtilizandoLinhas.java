6d91f81d3c194520735797f6a4da8c164da24523
Roger <rogerbritosan@gmail.com>
2023-01-26 10:15:01 -0300

    First commit

---
+++ CursoJavaNelioAlves/src/estrutura/sequencial/UtilizandoLinhas.java
@@ -0,0 +1,15 @@
+package estrutura.sequencial;
+
+import javax.swing.JOptionPane;
+
+public class UtilizandoLinhas {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Bem-vindo ao curso Java do Nelio Alves!");
+		System.out.print("Olá, ");
+		System.out.println(JOptionPane.showInputDialog("Digite seu nome:"));
+		
+	}
+	
+}
